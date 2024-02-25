126938b4079c122a1a6a434ab62296af16e03b6d
Axle <95949634+AxleCat284@users.noreply.github.com>
2024-02-25 06:43:17 -0500

    keep api from dying part infinity

--- routes/partyStatsRoute.js
+++ routes/partyStatsRoute.js
@@ -9,35 +9,38 @@ module.exports = wrap(async function (req, res) {
     }
     const networthRaw = await getSkyHelper(profileid, uuid2);
     let dungeons_data = profile?.dungeons
-
+    let run_data = dungeons_data?.last_dungeon_run
+    if (run_data === null || undefined) {
+        run_data = "No Run Found"
+    } 
     const catacombs = calcSkill("dungeoneering", dungeons_data?.dungeon_types?.catacombs?.experience.toString())
     return res.status(200).json({status: 200, data: 
         {
             player_uuid: uuid2,
             player_username: username,
-            player_profilename: profilename,
-            player_profileid: profileid,
-            total_networth: networthRaw.networth.networth.toString(),
-            magical_power: profile?.accessory_bag_storage.highest_magical_power,
-            selected_magical_power: profile?.accessory_bag_storage.selected_power,
+            player_profilename: profilename || "PROFILE_NAME_NOT_FOUND",
+            player_profileid: profileid || "NO_ID",
+            total_networth: networthRaw.networth.networth.toString() || 0,
+            magical_power: profile?.accessory_bag_storage.highest_magical_power || 0,
+            selected_magical_power: profile?.accessory_bag_storage.selected_power || "NO_POWER",
             dungeons: {
-                catacombs_secrets: playerData.player.achievements.skyblock_treasure_hunter,
-                catacombs_experience: catacombs,
-                last_floor: dungeons_data?.last_dungeon_run,
-                selected_class: dungeons_data?.selected_dungeon_class,
-                daily_completed_runs: dungeons_data?.daily_runs?.completed_runs_count,
+                catacombs_secrets: playerData.player.achievements.skyblock_treasure_hunter || 0,
+                catacombs_experience: catacombs || 0,
+                last_floor: run_data || 0,
+                selected_class: dungeons_data?.selected_dungeon_class || "NO_CLASS",
+                daily_completed_runs: dungeons_data?.daily_runs?.completed_runs_count || 0,
                 catacombs: {
                     fastest_times: {
-                        s_runs: dungeons_data?.dungeon_types?.catacombs?.fastest_time_s,
-                        s_plus_runs: dungeons_data?.dungeon_types?.catacombs?.fastest_time_s_plus,
-                        runs: dungeons_data?.dungeon_types?.catacombs?.fastest_time
+                        s_runs: dungeons_data?.dungeon_types?.catacombs?.fastest_time_s || 0,
+                        s_plus_runs: dungeons_data?.dungeon_types?.catacombs?.fastest_time_s_plus || 0,
+                        runs: dungeons_data?.dungeon_types?.catacombs?.fastest_time || 0
                         },
                     },
                     master_catacombs: {
                         fastest_times: {
-                            s_runs: dungeons_data?.dungeon_types?.master_catacombss?.fastest_time_s,
-                            s_plus_runs: dungeons_data?.dungeon_types?.master_catacombs?.fastest_time_s_plus,
-                            runs: dungeons_data?.dungeon_types?.master_catacombs?.fastest_time
+                            s_runs: dungeons_data?.dungeon_types?.master_catacombss?.fastest_time_s || 0,
+                            s_plus_runs: dungeons_data?.dungeon_types?.master_catacombs?.fastest_time_s_plus || 0,
+                            runs: dungeons_data?.dungeon_types?.master_catacombs?.fastest_time || 0
                             },
                     },
                 },
